21575dd33d55e8847a509355aad1b5800fc86c32
mvigneras <maxence.vigneras@gmail.com>
2017-11-17 21:31:58 -0500

    idk

--- Machine_learning_basics.py
+++ Machine_learning_basics.py
@@ -1,2 +1,60 @@
+from sklearn import preprocessing, svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import quandl
+import math
 import time
+from datetime import datetime as dt
+from matplotlib import style
+import matplotlib.pyplot as plt
+
+df = quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close'] ) / df ['Adj. Close'] * 100.0
+df['PCT_change']  = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+days_back = 0.01
+forecast_out = int(math.ceil(days_back*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+X = np.array(df.drop(['label'],1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out:]
+
+y = df['label']
+y.dropna(inplace=True)
+y = np.array(y)
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+
+clf=LinearRegression()
+clf.fit(X_train,y_train)
+accuracy=clf.score(X_test,y_test)
+forecast_set = clf.predict(X_lately)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = time.mktime(time.strptime(str(last_date), "%Y-%m-%d %H:%M:%S"))  # .timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = dt.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
 
